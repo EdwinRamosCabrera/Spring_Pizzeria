4652fdf334ef6a6498db3814a7f22e4fccfc6fbd
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-01-22 21:07:46 -0500

    Commit Inicial

---
+++ src/main/java/com/platzi/pizzeria_presto/PizzeriaPrestoApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.pizzeria_presto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaPrestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaPrestoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platzi/pizzeria_presto/PizzeriaPrestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.pizzeria_presto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaPrestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
