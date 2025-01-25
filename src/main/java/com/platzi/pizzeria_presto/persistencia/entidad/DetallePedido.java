939efcdb701215b206fbe2aaa06590ba762d2cab
Edwin Ramos <edwin_ramos2@usmp.pe>
2025-01-24 23:50:39 -0500

    Add: Construcción de relaciones entre entidades

--- src/main/java/com/platzi/pizzeria_presto/persistencia/entidad/DetallePedido.java
+++ src/main/java/com/platzi/pizzeria_presto/persistencia/entidad/DetallePedido.java
@@ -19,15 +19,26 @@ public class DetallePedido {
     @Column(name = "id_detalle_pedido")
     private Long detallePedidoId;
 
-    @Column(name = "id_pedido")
+    @Column(name = "id_pedido", nullable = false)
     private Long pedidoId;
 
-    @Column(name = "id_pizza")
+    @Column(name = "id_pizza", nullable = false)
     private Integer pizzaId;
 
+    @Column(nullable = false)
     private Integer cantidad;
 
+    @Column(nullable = false)
     private Double precio;
 
+    @Column(length = 200)
     private String comentarios;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido",referencedColumnName = "id_pedido", insertable = false, updatable = false)
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pizza", insertable = false, updatable = false)
+    private Pizza pizza;
 }
